8014e625a89a1bcfb72fc1fb27375a600c79f3a5
shahriar-bhuiyan <Shahriarbhuiyan1@gmail.com>
2024-05-27 02:47:34 +0600

    project files

---
+++ client/src/components/navbar/Navbar.jsx
@@ -0,0 +1,53 @@
+import "./navbar.css";
+import { Link } from "react-router-dom";
+import { useContext } from "react";
+import { AuthContext } from "../../context/AuthContext";
+const Navbar = () => {
+  const { user, dispatch } = useContext(AuthContext);
+
+  const handleLogout = () => {
+    dispatch({ type: "LOGOUT" });
+  };
+
+  return (
+    <div className="navbar">
+      <div className="navContainer">
+        <Link to="/" style={{ color: "inherit", textDecoration: "none" }}>
+          <span className="logo">ZaferTravel</span>
+        </Link>
+        {user ? (
+          <div className="navItems">
+            {/* <Link
+              to={"/book"}
+              style={{
+                margin: "5px",
+                color: "white",
+                padding: "5px 5px",
+                textDecoration: "none",
+                border: "1px solid white",
+                borderRadius: "15px",
+              }}
+            >
+              <span>Check your booking list</span>
+            </Link> */}
+            <span className="username">{user.username}</span>
+            <button className="navButton" onClick={handleLogout}>
+              Logout
+            </button>
+          </div>
+        ) : (
+          <div className="navItems">
+            <Link to={"register"}>
+              <button className="navButton">Register</button>{" "}
+            </Link>
+            <Link to={"login"}>
+              <button className="navButton">Login</button>{" "}
+            </Link>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
