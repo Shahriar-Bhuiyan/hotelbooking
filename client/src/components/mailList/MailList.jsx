670d303aeff6fff5df7b09e714f0a77af423e4c1
shahriar-bhuiyan <Shahriarbhuiyan1@gmail.com>
2024-06-02 01:51:16 +0600

    final

--- client/src/components/mailList/MailList.jsx
+++ client/src/components/mailList/MailList.jsx
@@ -1,16 +1,51 @@
-import "./mailList.css"
+import "./mailList.css";
+
+import Card from "react-bootstrap/Card";
+import Row from "react-bootstrap/esm/Row";
+import Col from "react-bootstrap/esm/Col";
+import Container from "react-bootstrap/esm/Container";
+import Zami from "../../assets/Zami.jpeg";
+import Sumit from "../../assets/Sumit.jpeg";
+import Towhid from "../../assets/Towhid.jpeg";
+
 
 const MailList = () => {
   return (
     <div className="mail">
-      <h1 className="mailTitle">Save time, save money!</h1>
-      <span className="mailDesc">Sign up and we'll send the best deals to you</span>
-      <div className="mailInputContainer">
-        <input type="text" placeholder="Your Email" />
-        <button>Subscribe</button>
-      </div>
-    </div>
-  )
-}
+      <Container>
+        <h1>Developed By</h1>
+        <Row>
+          <Col>
+            <Card style={{ width: "18rem",height:"30rem",overflow:'hidden' }}>
+              <Card.Img variant="top" src={Zami} style={{maxHeight:'40rem'}}/>
+              <Card.Body>
+                <Card.Title>Zami Ul Haque</Card.Title>
+              </Card.Body>
+            </Card>
+          </Col>
 
-export default MailList
+          <Col>
+            <Card style={{ width: "18rem",height:"30rem",overflow:'hidden' }}>
+              <Card.Img variant="top" src={Sumit} style={{height:'24rem',objectFit:'cover'}}/>
+              <Card.Body>
+                <Card.Title>Sumit Bosu </Card.Title>
+              </Card.Body>
+            </Card>
+          </Col>
+
+          <Col>
+            <Card style={{ width: "18rem",height:"30rem",overflow:'hidden' }}>
+              <Card.Img variant="top" src={Towhid} style={{height:'24rem',objectFit:'cover'}}/>
+              <Card.Body>
+                <Card.Title>Abdullah Al Towhid</Card.Title>
+              </Card.Body>
+            </Card>
+          </Col>
+
+        </Row>
+      </Container>
+    </div>
+  );
+};
+
+export default MailList;
